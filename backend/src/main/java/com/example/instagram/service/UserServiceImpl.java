d497d4045287796ff143a841c5eeb1c4b77e899e
hjjang <hyoje89@naver.com>
2025-02-18 16:25:04 +0900

    feat: 백엔드 폴더 구조 변경: instagram → backend 이동

--- backend/src/main/java/com/example/instagram/service/UserServiceImpl.java
+++ backend/src/main/java/com/example/instagram/service/UserServiceImpl.java
@@ -7,8 +7,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
